5340f6f887ffacb93476afb752dd6a48421b16d3
AndyHou <andyhou965@gmail.com>
2024-11-13 12:18:34 +0400

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,30 @@
+from typing import List
+from setuptools import find_packages, setup
+
+
+HYPEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path, "r") as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="mlops-pipeline",
+    version="0.0.1",
+    author="Andy Hou",
+    description="This is a package for MLOps pipeline",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
